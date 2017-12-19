fe16dd663a153a8f9f2c98824975f83c0987fc5e
Daan <daan.adams@student.ucll.be>
2017-12-19 11:22:07 +0100

    main method in application

---
+++ src/main/java/meetingrooms/Application.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meetingrooms;
+
+/**
+ *
+ * @author Daan
+ */
+public class Application {
+
+     public static void main(String[] args) throws Exception {
+         Service service = new Service();
+         service.printAppointmentsNow();
+    }
+    
+}
