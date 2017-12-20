855abe9a37ec82e236e471e1d906ccb72287d7a3
Daan <daan.adams@student.ucll.be>
2017-12-20 10:40:47 +0100

    refactor

--- src/main/java/ui/handlers/RequestHandlerFactory.java
+++ src/main/java/ui/handlers/RequestHandlerFactory.java
@@ -1,4 +1,4 @@
-package ui.RequestHandlers;
+package ui.handlers;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,21 +8,29 @@ import java.util.Set;
 import org.reflections.Reflections;
 
 import java.lang.annotation.*; 
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import meetingrooms.Service;
 
 public class RequestHandlerFactory{
 	
 	private Map<String, RequestHandler> handlers = new HashMap<>();
 
-	public RequestHandlerFactory(Service model) throws InstantiationException, IllegalAccessException{
-		Reflections reflections = new Reflections("view.handlers");
+	public RequestHandlerFactory(Service model){
+		Reflections reflections = new Reflections("ui.handlers");
 		Set<Class<?>> annotated = reflections.getTypesAnnotatedWith(Mapping.class);
 		for (Class<?> clazz : annotated) {
-			RequestHandler handler = (RequestHandler) clazz.newInstance();
-		    if(handler != null){
+                    RequestHandler handler;
+                    try {
+                        handler = (RequestHandler) clazz.newInstance();
+                        if(handler != null){
 				handler.setModel(model);
 				handlers.put(clazz.getAnnotation(Mapping.class).value(), handler);
 			}
+                    } catch (InstantiationException | IllegalAccessException ex) {
+                        Logger.getLogger(RequestHandlerFactory.class.getName()).log(Level.SEVERE, null, ex);
+                    }
+		    
 		}
 	}
 	
